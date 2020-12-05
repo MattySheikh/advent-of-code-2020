e4a85900aa26c7fb69f5310d7102d823636e7a8b
Matthew Al-Sheikh <matthewa@qualtrics.com>
2020-12-04 22:13:04 -0800

    Day two part two

--- cmd/day-2-part-2.go
+++ cmd/day-2-part-2.go
@@ -8,8 +8,16 @@ import (
 	"strconv"
 )
 
-// DayTwo runs day two
-func DayTwo() {
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const target = 2020
+
+// DayTwoPartTwo runs day two part two
+func DayTwoPartTwo() {
 	file, err := os.Open("../internal/day-2-input.txt")
 	check(err)
 	scanner := bufio.NewScanner(file)
@@ -19,15 +27,18 @@ func DayTwo() {
 	count := 0
 	for scanner.Scan() {
 		split := strings.Fields(scanner.Text())
-		numVals, ruleWithColon, str :=  split[0], split[1], split[2]
+		numVals, letterWithColon, str := split[0], split[1], split[2]
 		numValsSplit := strings.Split(numVals, "-")
 		min, _ := strconv.Atoi(numValsSplit[0])
 		max, _ := strconv.Atoi(numValsSplit[1])
 
-		rule := strings.Replace(ruleWithColon, ":", "", -1)
-		numOccurences := strings.Count(str, rule)
+		letter := strings.Replace(letterWithColon, ":", "", -1)
 
-		if numOccurences >= min && numOccurences <= max {
+		if (string(str[min - 1]) == letter) {
+			if (string(str[max - 1]) != letter) {
+				count++
+			}
+		} else if (string(str[max - 1]) == letter) {
 			count++
 		}
 	}
